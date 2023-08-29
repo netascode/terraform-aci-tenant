28959ab50dca8e6a47509bca2df0ba27e4010a44
Conor Murphy <conormurphy33@hotmail.com>
2023-08-29 14:48:22 +0200

    Adding security_domains for tenant resource

--- main.tf
+++ main.tf
@@ -7,3 +7,12 @@ resource "aci_rest_managed" "fvTenant" {
     descr     = var.description
   }
 }
+
+resource "aci_rest_managed" "aaaDomainRef" {
+  for_each   = toset(var.security_domains)
+  dn         = "uni/tn-${var.name}/domain-${each.value}"
+  class_name = "aaaDomainRef"
+  content = {
+    name = each.value
+  }
+}
